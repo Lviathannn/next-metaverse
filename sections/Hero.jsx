d9ae483c987f5f6516145049b10d2eb5483365f0
Lviathannn <valorant.leviathan@gmail.com>
2023-02-25 17:24:36 +0700

    Fix Typo

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -4,7 +4,7 @@ import style from "../styles";
 import { motion } from "framer-motion";
 
 const Hero = () => (
-   <section className={`${style.yPaddings} sm:pl-16 pl-6 text-white`}>
+   <section className={`${style.yPaddings} pl-6 text-white sm:pl-16`}>
       <motion.div
          className={`${style.innerWidth} mx-auto flex flex-col`}
          variants={staggerContainer}
@@ -12,7 +12,7 @@ const Hero = () => (
          whileInView="show"
          viewport={{ once: false, amount: 0.25 }}
       >
-         <div className="flex justify-center items-center flex-col relative z-10">
+         <div className="relative z-10 flex flex-col items-center justify-center">
             <motion.h1
                variants={textVariant(1.1)}
                className={style.heroHeading}
@@ -21,7 +21,7 @@ const Hero = () => (
             </motion.h1>
             <motion.div
                variants={textVariant(1.2)}
-               className="flex flex-row justify-center items-center"
+               className="flex flex-row items-center justify-center"
             >
                <h1 className={style.heroHeading}>Ma</h1>
                <h1 className={style.heroDText} />
@@ -30,20 +30,20 @@ const Hero = () => (
          </div>
          <motion.div
             variants={slideIn("right", "tween", 0, 2, 1)}
-            className="relative w-full md:-mt-[20px] -mt-[12px]"
+            className="relative -mt-[12px] w-full md:-mt-[20px]"
          >
-            <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px] rounded-tr-[20px]" />
+            <div className="hero-gradient absolute -top-[30px] z-[0] h-[300px] w-full rounded-tl-[140px] rounded-tr-[20px]" />
             <img
                src="/cover.png"
                alt="cover"
-               className="object-cover w-full sm:h-[500px] h-[30px] z-10 relative rounded-tl-[140px] rounded-b-[20px]"
+               className="relative z-10 h-[350px] w-full rounded-b-[20px] rounded-tl-[140px] object-cover sm:h-[500px]"
             />
             <a href="#explore">
-               <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+               <div className="relative z-10 -mt-[50px] flex w-full justify-end pr-[40px] sm:-mt-[70px]">
                   <img
                      src="/stamp.png"
                      alt="stamp"
-                     className="sm:w-[155px] w-[100px] sm:h-[155px] g-[100px] object-contain"
+                     className="g-[100px] w-[100px] object-contain sm:h-[155px] sm:w-[155px]"
                   />
                </div>
             </a>
